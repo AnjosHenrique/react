6ac124d23e1754924598d0fecf5afb99dc3279b5
AnjosHenrique <79482171+AnjosHenrique@users.noreply.github.com>
2021-07-30 15:54:27 -0300

    modificando app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Contador from './components/Contador';
 function App() {
   return (
     <div>
-      <h1>Olá Mundo React</h1>
+      <h1>Olá Mundo React testando github</h1>
       <Contador />
     </div>
   );
